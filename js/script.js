6209625a4a0c1def56a07b8af982f26830917841
BeatrizMachado <beatriz.machadosilva@gmail.com>
2021-09-23 23:02:47 -0300

    questionario dinamico
    criação do script para dar dinamismo ao questonario

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+var questionario = [
+    {
+        pergunta: "Oi, Tudo bem?",
+        option1: "Sim",
+        option2: "Não",
+        nextquestion1: 1,
+        nextquestion2: 2,
+    },
+    {
+        pergunta: "Porque?",
+        option1: "porque, Sim",
+        option2: "porque, Não",
+        nextquestion1: 0,
+        nextquestion2: 2,
+    },
+    {
+        pergunta: "Ta ok!",
+        option1: "ok",
+        option2: "Valeu",
+        nextquestion1: 0,
+        nextquestion2: 1,
+    },
+]
+
+
+
+
+
+
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
@@ -5,3 +34,21 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+
+function exibirpergunta(question){
+    $("#question").text(`${question.pergunta}`)
+    $("#resposta").html(`'<button class="btnresposta" id="sim">${question.option1}</button><button class="btnresposta" id="nao">${question.option2}</button>'`)
+    $("#sim").click(function(){
+        var idnext = question.nextquestion1
+        exibirpergunta(questionario[idnext])
+    });
+    $("#nao").click(function(){
+        var idnext = question.nextquestion2
+        exibirpergunta(questionario[idnext])
+    });
+}
+
+$( document ).ready(function() {
+    exibirpergunta(questionario[0])
+  });
